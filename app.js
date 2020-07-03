bb82c5f994989f0c1a1fca18e5c89d8c414d2f2a
luchi29 <lucifirma@gmail.com>
2020-07-02 21:07:37 -0300

    update

--- app.js
+++ app.js
@@ -11,9 +11,12 @@ var compression = require('compression');
 var helmet = require('helmet');
 
 var app = express();
+
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://Luchita29:performance29@cluster0-6tayg.azure.mongodb.net/<dbname>?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://cooluser:coolpassword@cluster0-mbdj7.mongodb.net/local_library?retryWrites=true'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true  });
+mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
